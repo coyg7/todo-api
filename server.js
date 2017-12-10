c39cb8345814282d7011d2a21a0e1357c5bbff69
Sachit Shrestha <ssachit3761@gmail.com>
2017-12-11 03:31:59 +0530

    Added post request

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [
-    {
-    id: 1,
-    description: 'Explore Node and React',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Sleep well stay healthy',
-    completed: true
-    }];
+var todos = [];
+var todoNextId = 1 ;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('ToDo API Root');
 });
 
+//GET requests
 app.get('/todos', function(req, res) {
    res.json(todos);
 });
@@ -39,8 +32,20 @@ app.get('/todos/:id', function(req, res) {
     } else {
         res.status(404).send();
     }
-   // res.send('Asking for todo with id of ' + req.params.id);
 });
+
+
+//POST /todos
+app.post('/todos', function(req, res) {
+    var body = req.body;
+
+    body.id = todoNextId++;
+    todos.push(body);
+    // console.log('description: ' + body.description);
+    res.json(body);
+});
+
+
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!!');
