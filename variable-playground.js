5e0f9c654aa64a2e29ad621257c77405f25267fc
Sachit Shrestha <ssachit3761@gmail.com>
2017-12-11 08:13:19 +0530

    Added query parameters request

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+//     name: 'Sachit',
+//     age: 16
+// };
+// const cl = 12;
+// function updateclass(){
+//     cl=50;
+// }
+// function updatePerson(obj) {
+//     obj.age = 22;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+//
+
+var grades = [15, 88];
+
+function addGrades(grades) {
+    grades.push(55);
+    debugger;
+}
+
+grades = addGrades(grades);
+console.log(grades);
