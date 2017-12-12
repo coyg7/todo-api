c3ce46ba44b17a573f5c1aec6b709898690a232e
Sachit Shrestha <ssachit3761@gmail.com>
2017-12-12 20:34:22 +0530

    Users model created with validations

--- server.js
+++ server.js
@@ -35,20 +35,6 @@ app.get('/todos', function (req, res) {
     }, function (e) {
         res.status(500).send();
     });
-    // var filteredTodos = todos;
-    //
-    // if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-    //     filteredTodos = _.where(filteredTodos, {completed: true});
-    // } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-    //     filteredTodos = _.where(filteredTodos, {completed: false});
-    // }
-    //
-    // if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    //     filteredTodos = _.filter(filteredTodos, function (todo) {
-    //         return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-    //     });
-    // }
-    // res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req, res) {
@@ -122,6 +108,16 @@ app.put('/todos/:id', function (req, res) {
         }
     }, function () {
         res.status(500).send();
+    });
+});
+
+app.post('/users', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function (user) {
+        res.json(user.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
     });
 });
 
