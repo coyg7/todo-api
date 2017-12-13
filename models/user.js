acf926a4ccb9a568ef4ff3ffde2bf6310ce025fb
Sachit Shrestha <ssachit3761@gmail.com>
2017-12-13 12:13:33 +0530

    Added auth token to POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('user', {
+    var user = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -35,10 +37,57 @@ module.exports = function (sequelize, DataTypes) {
     }, {
         hooks: {
             beforeValidate: function (user, options) {
-                if(typeof  user.email === 'string') {
+                if (typeof  user.email === 'string') {
                     user.email = user.email.toLowerCase();
                 }
             }
+        },
+        classMethods: {
+            authenticate: function (body) {
+                return new Promise(function (resolve, reject) {
+                    if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+                        return reject();
+                    }
+                    user.findOne({
+                        where: {
+                            email: body.email
+                        }
+                    }).then(function (user) {
+                        if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+                            return reject();
+                        }
+                        resolve(user);
+                        // res.header('Auth', user.generateToken('authentication')).json(user.toJSON());
+                    }, function (e) {
+                        reject();
+                    });
+                });
+            }
+        },
+        instanceMethods: {
+            toPublicJSON: function () {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+            },
+            generateToken: function (type) {
+                if (!_.isString(type)) {
+                    return undefined;
+                }
+                try {
+                    var stringData = JSON.stringify({id: this.get('id'), type: type});
+                    var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!0').toString();
+                    var token = jwt.sign({
+                        token: encryptedData
+                    }, 'qwerty098');
+                    return token;
+
+                } catch (e) {
+                    console.error(e);
+                    return undefined;
+                }
+            }
         }
+
     });
+    return user;
 };
--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.create(body).then(function (user) {
-        res.json(user.toJSON());
+        res.json(user.toPublicJSON());
     }, function (e) {
         res.status(400).json(e);
     });
@@ -124,22 +124,19 @@ app.post('/users', function (req, res) {
 
 //POST /users/login
 app.post('/users/login', function (req, res) {
-   var body = _.pick(req.body, 'email', 'password');
-   if(typeof body.email !== 'string' || typeof body.password !== 'string') {
-       return res.status(400).send();
-   }
-   db.user.findOne({
-       where: {
-           email: body.email
-       }
-   }).then(function (user) {
-        if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-            return res.status(401).send();
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function (user) {
+        var token = user.generateToken('authentication');
+
+        if (token) {
+            res.header('Auth', token).json(user.toPublicJSON());
+        } else {
+            res.status(401).send();
         }
-        res.json(user.toJSON());
-   }, function (e) {
-       res.status(500).send(e);
-   });
+    }, function () {
+        res.status(401).send();
+    });
 });
 
 
